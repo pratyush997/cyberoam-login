89554ba9f00d6d5890226cb71e5e79438e537686
pratyush997 <pratyush997z@gmail.com>
2017-03-20 08:10:43 +0530

    Fixed Initial Commit

---
+++ LoginApp/Credential.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginApp
+{
+    [DataContract]
+    public class Credential
+    {
+        [DataMember]
+        public string UserID { get; set; }
+
+        [DataMember]
+        public string Password { get; set;  }
+    }
+}
