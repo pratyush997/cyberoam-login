66a0b4d8264ba2ebf2bc3112031dd35ef4ac448f
pratyush997 <pratyush997z@gmail.com>
2017-03-20 07:42:58 +0530

    Initial Commit

---
+++ test1/LoginStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginApp
+{
+    public enum LoginStatus
+    {
+        LoggedIn,
+        LoggedOut,
+        WrongPassword,
+        MaxLimit,
+        LogInAgain,
+        DataLimit,
+        NoConnection
+    }
+}
---
+++ test1/Window1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoginApp
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+
+            if (Properties.Settings.Default.AutoLogin)
+            {
+                loginCheckBox.IsChecked = true;
+            }
+        }
+
+        private void loginCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (loginCheckBox.IsChecked.Value)
+                Properties.Settings.Default.AutoLogin = true;
+            else
+                Properties.Settings.Default.AutoLogin = false;
+            Properties.Settings.Default.Save();
+        }
+
+        private void hostButton_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.URL = hostBox.Text;
+            Properties.Settings.Default.Save();
+        }
+
+    }
+}
